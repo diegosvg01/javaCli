d571f40dcb5ce50f1e1b28ad125b090126dd0cde
Diego dos Santos <89267563+diegosvg01@users.noreply.github.com>
2022-05-26 18:02:46 -0300

    UPDATE

---
+++ assistentGUI.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo \"Criando usuário...\"
+sudo adduser quatro
+
+sudo usermod -aG sudo quatro
+
+sudo su quatro
+
+echo \"Iniciando atualização dos pacotes...\"
+sudo apt update && sudo apt upgrade -y
+
+echo \"Baixando Java 11\"
+sudo apt install default-jre
+sudo apt instal openjdk-11-jre-headlwss
+
+docker --version
+if [ $? -eq 0 ]
+then
+echo \"Docker já instalado!\"
+else
+echo \"Docker não instalado, gostaria de instalar? [s/n~]?\"
+read inst
+if [ \"$inst\" == \"s\" ]
+then
+echo \"Iniciando instalação do docker...\"
+sudo apt install docker.io -y
+echo \"Verificando imagem MySql\"
+sudo docker images | grep mysql
+if [ $? -eq 0 ]
+then
+echo \"MySql instalado\"
+else
+echo \"MySql não instalado, instalando...\"
+sudo docker pull mysql:5.7
+echo \"Iniciando docker.\"
+sleep 3
+sudo docker run -d -p 3306:3306 --name ContainerBD -e "MYSQL_DATABASE=4four" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
+fi
+else
+echo \"Você escolheu não instalar o docker\"
+fi
+fi
+
+
+cat > Dockerfile <<EOF
+FROM openjdk:11
+WORKDIR /javaCli/
+COPY /quatro_executable_gui.jar/ /javaCli/
+CMD ["java","-jar","quatro_executable_gui.jar"]
+EOF
+
+sudo docker build . -t quatro_executable_gui-jar
+
+sudo docker run -it quatro_executable_gui-jar
